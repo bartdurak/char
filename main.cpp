3e3601f87cb3682d2bb75f8f5c41ae27b63b6838
bartdurak <projektator@gmail.com>
2022-12-26 21:50:40 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+char myGrade ='B';
+cout << myGrade;
+char a=65, b=66, c=67, d=68, e=69;
+cout <<a;
+cout <<b;
+cout <<c;
+cout <<d;
+cout <<e;
+
+return 0;
+}
